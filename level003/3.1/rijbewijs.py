f8d73a124c47e36f2faacf44fb42d60f154e5752
Jacob Pieter Strampel <127293336+JpStrampel@users.noreply.github.com>
2024-12-04 11:52:54 +0100

    Delete level003 directory

--- level003/3.1/rijbewijs.py
+++
@@ -1,9 +0,0 @@
-print('Hoe oud bent u?')
-leeftijd = float(input())  # Taking input as a float, as age could be a decimal number
-
-MinLeeftijd = 16.5  # Minimum age to start driving lessons
-
-if leeftijd >= MinLeeftijd:
-    print("U kunt beginnen met rijles.")
-else:
-    print("U mag nog niet beginnen met rijles.")
--- level003/3.2/faculty.py
+++
@@ -1,5 +0,0 @@
-def faculteit(n):
-    return 1 if n == 0 else n * faculteit(n - 1)
-
-getal = int(input("Voer een positief geheel getal in: "))
-print(f"De faculteit van {getal} is: {faculteit(getal)}")
